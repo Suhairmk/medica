0c610404d9b254cccf6af5de694b4936e0406e2e
Sharafudheenkamar <sharafudheenkamar@gmail.com>
2026-02-01 15:10:14 +0530

    password hashing in django admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,32 @@
-
-
-# Register your models here.
 from django.contrib import admin
-from .models import User
-from .models import PatientProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, PatientProfile
+from .forms import UserCreateForm
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role', 'is_active', 'is_staff')
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = UserCreateForm  # Use your custom form for creating users
+    model = User
+
+    list_display = ('username', 'email', 'phone', 'role', 'is_active', 'is_staff')
     list_filter = ('role', 'is_active')
+
     search_fields = ('username', 'email', 'phone')
+
+    # Fields for adding a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone', 'role', 'password', 'is_active', 'is_staff'),
+        }),
+    )
+
+    # Fields for editing an existing user
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone', 'role', 'password', 'is_active', 'is_staff')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+
 
 @admin.register(PatientProfile)
 class PatientProfileAdmin(admin.ModelAdmin):
--- users/forms.py
+++ users/forms.py
@@ -1,23 +1,46 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
 
 class UserCreateForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+        strip=False,
+    )
 
     class Meta:
         model = User
-        fields = [
-            'username',
-            'email',
-            'phone',
-            'role',
-            'password',
-            'is_active',
-        ]
+        fields = ['username', 'email', 'phone', 'role', 'password', 'is_active']
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        password = self.cleaned_data["password"]
+        if password:
+            user.set_password(password)  # Hash password
         if commit:
             user.save()
         return user
+
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all the fields on
+    the user, but replaces the password field with a display
+    field that shows the hashed password.
+    """
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text="Raw passwords are not stored, so there is no way to see this user's password, "
+                  "but you can change the password using <a href='../password/'>this form</a>."
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'role', 'password', 'is_active', 'is_staff']
+
+    def clean_password(self):
+        # Always return the initial value regardless of what the user provides
+        return self.initial["password"]
